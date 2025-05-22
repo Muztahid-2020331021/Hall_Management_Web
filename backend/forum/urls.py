4df525957d98ace8360e65734ba9116ee26b0062
Sheikh Khalid Azad <107164227+Khalidgithub2020331007@users.noreply.github.com>
2025-05-22 14:48:36 +0600

    Change in forum urls.py

--- backend/forum/urls.py
+++ backend/forum/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ForumListCreateView
 
 urlpatterns = [
-    path('/', ForumListCreateView.as_view(), name='forum-list-create'),
+    path('forum/', ForumListCreateView.as_view(), name='forum-list-create'),
 ]
