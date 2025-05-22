de7913bab0e64e777f13d377ea33a8f635514870
Sheikh Khalid Azad <107164227+Khalidgithub2020331007@users.noreply.github.com>
2025-05-22 07:27:42 +0600

    Add files via upload
    add another from backed

---
+++ backend/backend/urls.py
@@ -0,0 +1,20 @@
+from django.conf import settings  # Import settings
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('registration/', include('registration.urls')),
+    path('complain/', include('complain.urls')),
+    path('guest_registration/', include('guest_registration.urls')),
+    path('notice_board/', include('notice_board.urls')),
+    path('lost_and_found/', include('lost_and_found.urls')),
+    path('meetings/', include('meetings.urls')),
+    path('events/', include('events.urls')),
+    path('official_transaction/', include('official_transaction.urls')),
+    path('forum/', include('forum.urls')),
+]
+
+if settings.DEBUG:  # Now you can use settings.DEBUG
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
