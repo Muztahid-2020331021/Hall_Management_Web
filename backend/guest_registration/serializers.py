fbeb57828c6774fe0a3081b06374e76b3f2054be
Muztahid-2020331021 <sajid191693@gmail.com>
2025-06-03 16:33:03 +0600

    Fixed serializers.py file of guest_registration

--- backend/guest_registration/serializers.py
+++ backend/guest_registration/serializers.py
@@ -1,14 +1,18 @@
+# =================
+# guest_registration serializers.py
+# =================
+
 from rest_framework import serializers
 from .models import *
 
-class GuestRegistrationSerializer(serializers.ModelSerializer):
+class GuestSerializer(serializers.ModelSerializer):
     reference_name=serializers.CharField(source='reference_email.name',read_only=True)
     reference_role=serializers.CharField(source='reference_email.user_role',read_only=True)
     guard_name=serializers.CharField(source='guard_email.name',read_only=True)
     hall=serializers.CharField(source='guard_email.hall',read_only=True)
 
     class Meta:
-        model=GuestRegistration
+        model=Guest
         fields=[
         'guest_id',
         'guest_name',
