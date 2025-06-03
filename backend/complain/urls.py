06d2dfb4aae0c6e88f77e099fe62df9cd3a970c7
Muztahid-2020331021 <sajid191693@gmail.com>
2025-06-03 16:06:58 +0600

    Updated urls.py file of complain app

--- backend/complain/urls.py
+++ backend/complain/urls.py
@@ -1,10 +1,14 @@
+# =================
+# Complain urls.py
+# =================
+
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ComplainViewSet,SystemGoalViewSet
+from .views import Make_ComplaintsViewSet, SystemGoalViewSet
 
 router = DefaultRouter()
-router.register('complain', ComplainViewSet, basename='complain')
-router.register('systemgoal', SystemGoalViewSet, basename='systemgoal')
+router.register(r'make_complaints', Make_ComplaintsViewSet, basename='make_complaints')
+router.register(r'system_goal', SystemGoalViewSet, basename='system_goal')
 
 urlpatterns = [
     path('', include(router.urls)),
