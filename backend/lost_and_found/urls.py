c9fb744918ec2f1e27d1be9f218ed8125605ad2a
Muztahid-2020331021 <sajid191693@gmail.com>
2025-06-03 16:58:51 +0600

    Updated urls.py file of lost_and_found

--- backend/lost_and_found/urls.py
+++ backend/lost_and_found/urls.py
@@ -1,3 +1,7 @@
+# =================
+# lost_and_found urls.py
+# =================
+
 # lostandfound/urls.py
 
 from django.urls import path, include
@@ -5,7 +9,7 @@ from rest_framework.routers import DefaultRouter
 from .views import LostAndFoundViewSet
 
 router = DefaultRouter()
-router.register(r'lost-and-found', LostAndFoundViewSet, basename='lost-and-found')
+router.register(r'new_lost_and_found', LostAndFoundViewSet, basename='lost-and-found')
 
 urlpatterns = [
     path('', include(router.urls)),
