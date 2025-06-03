c1b7249f029028d89a62e6c4bdb531801281083d
Muztahid-2020331021 <sajid191693@gmail.com>
2025-06-03 16:57:47 +0600

    Updated views.py file of lost_and_found

--- backend/lost_and_found/views.py
+++ backend/lost_and_found/views.py
@@ -1,9 +1,13 @@
+# =================
+# lost_and_found views.py
+# =================
+
 from django.shortcuts import render
 
 from rest_framework import viewsets
-from .models import LostAndFound
+from .models import New_LostAndFound
 from .serializers import LostAndFoundSerializer
 
 class LostAndFoundViewSet(viewsets.ModelViewSet):
-    queryset = LostAndFound.objects.all().order_by('-post_date_time')
+    queryset = New_LostAndFound.objects.all().order_by('-post_date_time')
     serializer_class = LostAndFoundSerializer
